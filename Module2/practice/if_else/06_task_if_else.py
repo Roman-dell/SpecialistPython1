98001aaae4602eb7727dda4e755fb20f93570614
Roman-dell <91017879+Roman-dell@users.noreply.github.com>
2021-09-26 12:09:06 +0300

    Update 06_task_if_else.py

--- Module2/practice/if_else/06_task_if_else.py
+++ Module2/practice/if_else/06_task_if_else.py
@@ -6,5 +6,8 @@
 # Формат выходных данных: Выведите количество дней в данном году.
 
 year = int(input("Год: "))
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print('366')
+else:
+    print('365')
 
-# TODO: your code here
