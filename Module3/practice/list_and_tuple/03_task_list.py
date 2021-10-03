ee346285e516e5745c73512bebc7c9c8d91d854b
Roman-dell <91017879+Roman-dell@users.noreply.github.com>
2021-10-03 12:31:57 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,14 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+import random
+
+numbers = []
+n = int(input("n:"))  # колличество
+i = 0
+while i < n:
+    number = random.randint(-10,10)  # Диапозон произвольных чисел.
+    numbers.append(number)
+    i += 1
+
+print(sum(numbers))
