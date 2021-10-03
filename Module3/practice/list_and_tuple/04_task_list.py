ee61b736f30e685eb7f369d031249ef705863e0b
Roman-dell <91017879+Roman-dell@users.noreply.github.com>
2021-10-03 12:34:56 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,18 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+import random
+
+numbers = []
+n = int(input("n:"))  # колличество
+i = 0
+while i < n:
+    number = random.randint(-10,10)  # Диапозон произвольных чисел.
+    numbers.append(number)
+    i += 1
+print(numbers)
+
+for el in numbers:
+    if el > 0:
+        el+=el
+print(el)
