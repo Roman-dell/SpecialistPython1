7e009d912d2496c3c2fe40cded1cb5cf11874fef
Roman-dell <91017879+Roman-dell@users.noreply.github.com>
2021-10-10 15:14:12 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -2,6 +2,13 @@
 # Напишите функцию, проверяющую является ли число палиндромом.
 # палиндром - число одинаково читающееся слева направо, так и справа налево.
 #  Пример палиндрома: 12321
-
 def palindrome(number):
-    pass
+    r_number = number
+    result = 0
+
+    while number != 0:
+        digit = number % 10
+        result = result * 10 + digit
+        number = int(number / 10)
+
+    return result == r_number
